ae6111e109bf8092ca387983313f62bc9623ebf0
JuanJoseIslasLopez <jujojo97@gmail.com>
2024-01-17 01:29:52 -0300

    w01 ready

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,30 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    
+    {   Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+
+         while (guess != magicNumber)
+        {   Console.Write("What is the magic number? ");
+            string inputTwo = Console.ReadLine();
+            guess = int.Parse(inputTwo);
+        
+            if (magicNumber == guess)
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            else if(magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+            else
+                {
+                    Console.WriteLine("Lower");
+                }
+        }
     }
 }
