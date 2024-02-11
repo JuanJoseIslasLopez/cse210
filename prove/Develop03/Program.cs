35065ef06a8ae3ccc4b3f5ca41519cdd274426b5
Juan José Islas Lopez <jujojo97@gmail.com>
2024-02-11 03:02:27 -0300

    w3 ready

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -3,7 +3,31 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Develop03 World!");
+    {   
+        Reference reference = new Reference("John", 3, 16);
+        Scripture scripture = new Scripture(reference, "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
+        
+        Console.WriteLine(scripture.GetDisplayText());
+        
+        Console.WriteLine("Press enter to continue or type 'quit' to finish.");
+
+        while (!scripture.IsCompletelyHidden()) {
+            
+            string input = Console.ReadLine();
+            if (input == "quit") {
+                break;
+                
+            }
+            
+           
+            scripture.HideRandomWords(1);
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine("Press enter to continue or type 'quit' to finish.");
+            
+        }
+
+        Console.WriteLine("Program ended.");
+    
     }
 }
---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,25 @@
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+     public Scripture(Reference reference, string text) {
+        _reference = reference;
+        _words = text.Split(' ').Select(word => new Word(word)).ToList();
+    }
+
+    public void HideRandomWords(int numberToHide) {
+        Random random = new Random();
+        for (int i = 0; i < numberToHide; i++) {
+            int index = random.Next(_words.Count);
+            _words[index].Hide();
+        }
+    }
+
+    public string GetDisplayText() {
+        return $"{_reference.GetDisplayText()}: {_words.Aggregate("", (current, word) => current + word.GetDisplayText() + " ")}";
+    }
+
+    public bool IsCompletelyHidden() {
+        return _words.All(word => word.IsHidden());
+    }
+}
