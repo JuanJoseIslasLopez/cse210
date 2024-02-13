2ff24f2af7daea31094851d2443f6de08bcfd537
Juan José Islas Lopez <jujojo97@gmail.com>
2024-02-13 02:15:57 -0300

    w 4 prepare reday

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignments assignments1 = new Assignments("Samuel Bennett","Multiplication");
+        Console.WriteLine(assignments1.GetSummary());
+
+        MathAssignments mathAssignments2 = new MathAssignments("Roberto Rodriguez","Fractions","Section 7.3","Problems 8-19");
+        Console.WriteLine(mathAssignments2.GetSummary());
+        Console.WriteLine(mathAssignments2.GetHomeworkList());
+
+        WritingAssignments writingAssignments3 = new WritingAssignments("Mary Waters","European History","The Causes of World War II by Mary Waters");
+        Console.WriteLine(writingAssignments3.GetSummary());
+        Console.WriteLine(writingAssignments3.GetWritingInformation());
     }
 }
---
+++ prepare/Learning04/WritingAssignments.cs
@@ -0,0 +1,14 @@
+public class WritingAssignments : Assignments
+{
+    private string _title;
+
+    public WritingAssignments(string studentName, string topic, string title): base(studentName,topic)
+    {   
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        return $"{_title}";
+    }
+}
